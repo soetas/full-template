9a09050271cfe8c821fdb83a3c3969f8f12525b5
sgk <3228891558@qq.com>
2024-04-28 09:21:50 +0800

    feat: modify some files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig(()=>{
   return {
     plugins:[
-      react()
+      svelte()
     ]
   }
 })

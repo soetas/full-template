b05d6a20b18f15e7a3da158cf00de20cf15881bd
sgk <3228891558@qq.com>
2024-04-28 09:10:32 +0800

    feat: modify some files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig(()=>{
   return {
     plugins:[
-      vue()
+      react()
     ]
   }
 })
